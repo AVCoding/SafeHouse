78514681683eae5c9367b116e4ba0f0b35061199
AVCoding <70842251+AVCoding@users.noreply.github.com>
2021-04-01 17:36:15 +0400

    Update

--- js/script.js
+++ js/script.js
@@ -37,9 +37,7 @@ jQuery(document).ready(function($) {
             var hash = this.hash;
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 1200, function(){
-                window.location.hash = hash;
-            });
+            }, 1200);
         } 
     });
 
